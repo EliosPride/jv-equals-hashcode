3651a52332d144482b83dc5f33d1bee23623c33a
Illia Sydorchuk <batonvagon@gmail.com>
2022-04-21 19:54:07 +0300

    Initial commit

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+/**
+ * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
+ * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
+ * а hashCode() был бы согласованным с реализацией equals().</p>
+ */
+public class Rectangle {
+    private Integer width;
+    private Integer length;
+    private String color;
+
+    public Rectangle(Integer width, Integer length, String color) {
+        this.width = width;
+        this.length = length;
+        this.color = color;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
